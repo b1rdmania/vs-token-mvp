d85be5273ae5bb4faee8896e5809a28c68186c1a
chirag0264 <chirag@tokentraxx.com>
2025-07-24 19:03:29 +0530

    ui_fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { sonicMainnet, sonicTestnet } from './config/chains';
 import { AppShell } from './components/layout/AppShell';
 import { LandingPage } from './pages/LandingPage';
 import { WhitepaperPage } from './pages/WhitepaperPage';
+import { Toaster } from 'react-hot-toast';
 
 import '@rainbow-me/rainbowkit/styles.css';
 
@@ -31,19 +32,24 @@ const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/whitepaper" element={<WhitepaperPage />} />
-              <Route path="/app/*" element={<AppShell />} />
-  
-            </Routes>
-          </Router>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<LandingPage />} />
+                <Route path="/whitepaper" element={<WhitepaperPage />} />
+                <Route path="/app/*" element={<AppShell />} />
+
+              </Routes>
+            </Router>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
   );
 }
+
+export default App;
